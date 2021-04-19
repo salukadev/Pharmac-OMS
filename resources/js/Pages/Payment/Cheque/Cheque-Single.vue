e1541c51774fb7ea3db2089497d33e23830b4a2c
Danuja Wijerathna <danujawijerathne45@gmail.com>
2021-04-19 16:27:20 +0530

    add ui template

--- resources/js/Pages/Payment/Cheque/Cheque-Single.vue
+++ resources/js/Pages/Payment/Cheque/Cheque-Single.vue
@@ -1,36 +1,190 @@
 <template>
     <Layout>
-        <div>
-            <p v-for="error in errors">{{error}}</p>
-            <form @submit.prevent="update">
-                <label for="frontImg">Front Image :</label>
-                <img v-bind:src="cheque.frontImg" class="thubnailImg">
-                <input type="file" @input="form.frontImg = $event.target.files[0]"> <br>
-                <label for="backImg">Front Image :</label>
-                <img v-bind:src="cheque.backImg" class="thubnailImg">
-                <input type="file" @input="form.backImg = $event.target.files[0]"> <br>
-                <input type="number" v-model="form.chequeNo">
-                <input type="number" v-model="form.payment_id">
-                <input type="date" v-model="form.chequeDate">
-                <input type="text" v-model="form.remark" readonly>
-                <input type="number" v-model="form.agent_id" readonly>
-                <input type="text" v-model="form.agent_Note" readonly>
-                <input type="text" v-model="form.created_at" readonly>
-                <input type="text" v-model="form.updated_at" readonly> <br>
-                <label for="admin_Note">Admin Note :</label>
-                <input type="text" v-model="form.admin_Note">
-                <input type="text" v-model="form.admin_id" hidden>
-                <button type="submit">Update</button>
-            </form>
-            <button @click="accept">Accept</button>
-            <button @click="reject">Reject</button>
-            <button @click="delete">Delete</button>
-        </div>
+        <v-app>
+            <div align="center">
+                <v-card  align="center" max-width="70%">
+                    <p v-for="error in errors">{{error}}</p>
+                    <form @submit.prevent="update" class="pb-8">
+                        <v-img
+                            v-bind:src="cheque.frontImg"
+                            contain
+                            max-width="100%"
+                            max-height="600px"
+                            class= "grey darken-0"
+                        ></v-img>
+                        <v-card-title>
+                            <v-row>
+                                <v-col cols="9" sm="6">
+                                    Front Image
+                                </v-col>
+                                <v-col cols="12" sm="4">
+                                    <v-file-input
+                                        @input="form.frontImg = $event.target.files[0]"
+                                        @change="prev_frontImg"
+                                        v-model="form.frontImg"
+                                        label="Update Front Image"
+                                        filled
+                                        prepend-icon="mdi-camera"
+                                    ></v-file-input>
+
+                                </v-col>
+                                <v-col>
+                                    <v-img :src="frontImg" max-height="200px" max-width="100px"></v-img>
+                                </v-col>
+                            </v-row>
+                        </v-card-title>
+                        <v-img
+                            v-bind:src="cheque.backImg"
+                            contain
+                            max-width="100%"
+                            max-height="600px"
+                            class="grey darken-0"
+                        ></v-img>
+                        <v-card-title>
+                            <v-row>
+                                <v-col cols="9" sm="6">
+                                    Back Image
+                                </v-col>
+                                <v-col cols="12" sm="4">
+                                    <v-file-input
+                                        @input="form.backImg = $event.target.files[0]"
+                                        @change="prev_backImg"
+                                        v-model="form.backImg"
+                                        label="Update Front Image"
+                                        filled
+                                        prepend-icon="mdi-camera"
+                                    ></v-file-input>
+                                </v-col>
+                                <v-col>
+                                    <v-img :src="backImg" max-height="200px" max-width="100px"></v-img>
+                                </v-col>
+                            </v-row>
+                        </v-card-title>
+                            <div class="m-4">
+                                <v-row>
+                                    <v-col cols="12"
+                                           sm="6">
+                                        <v-text-field
+                                            v-model="form.chequeNo"
+                                            type="number"
+                                            :error-messages="chequeNumberErrors"
+                                            label="Enter Cheque Number"
+                                            required
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12"
+                                           sm="6">
+                                        <input type="date" v-model="form.chequeDate">
+                                    </v-col>
+                                </v-row>
+
+                                <v-row>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                    >
+                                        <v-text-field
+                                            v-model="form.agent_id"
+                                            type="number"
+                                            :error-messages="pchequeNumberErrors"
+                                            label="Agent Id"
+                                            readonly
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                    >
+                                        <v-text-field
+                                            v-model="form.payment_id"
+                                            type="number"
+                                            :error-messages="chequeNumberErrors"
+                                            label="Enter Payment ID"
+                                            required
+                                        ></v-text-field>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                    >
+                                        <v-text-field
+                                            v-model="form.created_at"
+                                            :error-messages="pchequeNumberErrors"
+                                            label="Submit Date"
+                                            readonly
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                    >
+                                        <v-text-field
+                                            v-model="form.updated_at"
+                                            :error-messages="pchequeNumberErrors"
+                                            label="Last Update"
+                                            readonly
+                                        ></v-text-field>
+                                    </v-col>
+                                </v-row>
+                                <v-textarea
+                                    v-model="form.agent_Note"
+                                    filled
+                                    label="Agent Note"
+                                    auto-grow
+                                    readonly
+                                    value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+                                ></v-textarea>
+                                <v-textarea
+                                    v-model="form.admin_Note"
+                                    filled
+                                    label="Agent Note"
+                                    auto-grow
+                                    counter="255"
+                                    required
+                                    value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+                                ></v-textarea>
+                                <input type="text" v-model="form.admin_id" hidden>
+                                <!--                        <label for="frontImg">Front Image :</label>-->
+                                <!--                        <img v-bind:src="cheque.frontImg" class="thubnailImg">-->
+                                <!--                        <input type="file" @input="form.frontImg = $event.target.files[0]"> <br>-->
+                                <!--                        <label for="backImg">Front Image :</label>-->
+                                <!--                        <img v-bind:src="cheque.backImg" class="thubnailImg">-->
+                                <!--                        <input type="file" @input="form.backImg = $event.target.files[0]"> <br>-->
+                                <!--                        <input type="number" v-model="form.chequeNo">-->
+                                <!--                        <input type="number" v-model="form.payment_id">-->
+
+                                <!--                        <input type="text" v-model="form.remark" readonly>-->
+                                <!--                        <input type="number" v-model="form.agent_id" readonly>-->
+                                <!--                        <input type="text" v-model="form.agent_Note" readonly>-->
+                                <!--                        <input type="text" v-model="form.created_at" readonly>-->
+                                <!--                        <input type="text" v-model="form.updated_at" readonly> <br>-->
+                                <!--                        <label for="admin_Note">Admin Note :</label>-->
+                                <!--                        <input type="text" v-model="form.admin_Note">-->
+
+                            </div>
+                            <v-btn
+                                class="mr-4"
+                                type="submit"
+                                color="primary"
+                                dark
+                            >
+                                Update
+                            </v-btn>
+                            <v-btn type="button" class="mr-4" color="green" dark @click="accept">Accept</v-btn>
+                            <v-btn type="button" class="mr-4" color="error" dark @click="reject">Reject</v-btn>
+                            <v-btn type="button" class="mr-4" color="amber" dark @click="dlt">Delete</v-btn>
+                    </form>
+
+                </v-card>
+            </div>
+        </v-app>
     </Layout>
 </template>
 
 <script>
-import Layout from '../../../Shared/Layout'
+import Layout from "../../../Shared/Admin/Layout";
 
 export default {
     components: {
@@ -69,8 +223,14 @@ export default {
         update: function () {
             this.$inertia.post('/update',this.form)
         },
-        delete: function () {
+        dlt: function () {
             this.$inertia.post('/delete',this.form)
+        },
+        prev_frontImg(){
+            this.frontImg = URL.createObjectURL(this.form.frontImg)
+        },
+        prev_backImg(){
+            this.backImg = URL.createObjectURL(this.form.backImg)
         }
     }
 }
--- resources/js/Pages/Payment/Cheque/Cheque-Upload.vue
+++ resources/js/Pages/Payment/Cheque/Cheque-Upload.vue
@@ -1,24 +1,149 @@
 <template>
     <Layout>
-        <div>
-            <p v-for="error in errors">{{error}}</p>
-            <form @submit.prevent="submit">
-                <input type="file"   @input="form.frontImg = $event.target.files[0]">
-                <input type="file"   @input="form.backImg = $event.target.files[0]">
-                <input type="number" v-model="form.chequeNo" placeholder="Enter Cheque Nummber">
-                <input type="number" v-model="form.payment_id"  placeholder="Enter Payment Id">
-                <input type="date" v-model="form.chequeDate" placeholder="Date of Cheque">
-                <input type="text" v-model="form.remark" placeholder="Add Remark Here">
-                <input type="number" v-model="form.agent_id" placeholder="Agent Id">
-                <input type="text" v-model="form.agent_Note" placeholder="Agent note">
-                <button id="btnSubmit" type="submit">Submit</button>
-            </form>
-        </div>
+        <v-app>
+            <div align="center" marg>
+                <p v-for="error in errors">{{error}}</p>
+                <div class="card elevation-0">
+                    <div class="card-header card-header-warning card-header-icon" >
+                        <div class="card-icon">
+                            <i class="material-icons">upload</i>
+                        </div>
+                        <h4 align="left" class="card-title">Cheque Portal</h4>
+                        <v-card class="mt-10 mb-10" max-width="95%">
+
+                            <form class="p-5" @submit.prevent="submit">
+                                <v-row>
+                                    <v-col>
+                                        <v-img :src="url1" max-width="400px" max-height="200px" contain></v-img>
+                                    </v-col>
+                                    <v-col>
+                                        <v-img :src="url2" max-width="400px" max-height="200px" contain></v-img>
+                                    </v-col>
+                                </v-row>
+
+                                <v-row>
+                                    <v-col cols="12"
+                                           sm="6">
+                                        <v-file-input
+                                            @input="form.frontImg = $event.target.files[0]"
+                                            @change="Preview_image1"
+                                            v-model="form.frontImg"
+                                            label="Input Front Image"
+                                            filled
+                                            prepend-icon="mdi-camera"
+                                        ></v-file-input>
+                                    </v-col> <v-col cols="12"
+                                                    sm="6">
+                                    <v-file-input
+                                        @change="Preview_image2"
+                                        @input="form.backImg = $event.target.files[0]"
+                                        v-model="form.backImg"
+                                        label="Input Back Image"
+                                        filled
+                                        prepend-icon="mdi-camera"
+                                    ></v-file-input>
+                                </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col cols="12"
+                                           sm="6">
+                                        <v-text-field
+                                            v-model="form.chequeNo"
+                                            type="number"
+                                            :error-messages="chequeNumberErrors"
+                                            label="Enter Cheque Number"
+                                            required
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                    >
+                                        <v-menu
+                                            v-model="menu2"
+                                            :close-on-content-click="false"
+                                            :nudge-right="40"
+                                            transition="scale-transition"
+                                            offset-y
+                                            min-width="auto"
+                                        >
+                                            <template v-slot:activator="{ on, attrs }">
+                                                <v-text-field
+                                                    v-model="form.chequeDate"
+                                                    label="Select Cheque Date"
+                                                    prepend-icon="mdi-calendar"
+                                                    readonly
+                                                    v-bind="attrs"
+                                                    v-on="on"
+                                                ></v-text-field>
+                                            </template>
+                                            <v-date-picker
+                                                v-model="form.chequeDate"
+                                                @input="menu2 = false"
+                                            ></v-date-picker>
+                                        </v-menu>
+                                    </v-col>
+                                </v-row>
+
+                                <v-row>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                    >
+                                        <v-text-field
+                                            v-model="form.agent_id"
+                                            type="number"
+                                            :error-messages="pchequeNumberErrors"
+                                            label="Agent Id"
+                                            required
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                        cols="12"
+                                        sm="6"
+                                    >
+                                        <v-text-field
+                                            v-model="form.payment_id"
+                                            type="number"
+                                            :error-messages="chequeNumberErrors"
+                                            label="Enter Payment ID"
+                                            required
+                                        ></v-text-field>
+                                    </v-col>
+                                </v-row>
+                                <v-textarea
+                                    v-model="form.agent_Note"
+                                    filled
+                                    label="Agent Note"
+                                    auto-grow
+                                    counter="255"
+                                    value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+                                ></v-textarea>
+
+                                <v-btn
+                                    class="mr-4"
+                                    type="submit"
+                                    color="green"
+                                    dark
+                                >
+                                    submit
+                                </v-btn>
+                                <v-btn dark type="reset" color="red accent-2">
+                                    clear
+
+                                </v-btn>
+                            </form>
+                        </v-card>
+                    </div>
+                </div>
+            </div>
+        </v-app>
+
     </Layout>
 </template>
 
 <script>
-import Layout from '../../../Shared/Layout'
+import Layout from "../../../Shared/Admin/Layout";
 
 export default {
     components:{
@@ -46,6 +171,16 @@ export default {
             this.$inertia.post('/upload/store',this.form).then(()=>{
                 this.loading = "False"
             });
+        },
+        clear: function () {
+            this.$v.$reset()
+
+        },
+        Preview_image1() {
+            this.url1= URL.createObjectURL(this.form.frontImg)
+        },
+        Preview_image2() {
+            this.url2= URL.createObjectURL(this.form.backImg)
         }
     }
 
