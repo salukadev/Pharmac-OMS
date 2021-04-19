9d367928b34f7e22e4222297819811a19fc94147
Parami <lelkadaparami@gmail.com>
2021-04-19 13:11:46 +0530

    'supplier_store_function'

--- routes/web.php
+++ routes/web.php
@@ -116,6 +116,7 @@ Route::post('/updateProducts',[ProductListController::class,'update']);
 
 Route::get('/supplier', [SupplierController::class, 'index']);
 Route::post('/supplier/store',[SupplierController::class,'store']);
+Route::get('/sup', function () {return Inertia::render('Admin/Supplier/test',[]); });
 
 
 
