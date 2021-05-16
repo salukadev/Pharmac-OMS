f7cf2154a76965b6b3a5c989b5c0be9d7cc8e161
Danuja Wijerathna <danujawijerathne45@gmail.com>
2021-05-16 19:15:03 +0530

    setup financial dashboard

--- resources/js/Pages/Charts/BarChart.js
+++ resources/js/Pages/Charts/BarChart.js
@@ -5,9 +5,8 @@ export default {
     extends: Bar,
     mixins: [reactiveProp],
     props: ['options'],
+
     mounted () {
-        // this.chartData is created in the mixin.
-        // If you want to pass options please create a local options object
         this.renderChart(this.chartData, this.options)
     }
 }
