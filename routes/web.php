6f17c1dca6d4d6bba92747a20179388a7e2a08ce
SALUKA <salukadev4@gmail.com>
2021-05-16 15:25:15 +0530

    Storefront layout and css localization

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/dashboard', function () {
-    return Inertia::render('FinancialDashboard');
+    return Inertia::render('Home');
 });
 
 Route::get('/userProfile', function () {
@@ -67,7 +67,6 @@ Route::get('/register', function () {
 //Route::get('/list', function () {
 //    return Inertia::render('List',[]);
 //});
-
 Route::resource('orders', OrderController::class);
 
 //Agent Routes
@@ -94,7 +93,6 @@ Route::post('/category/store', [CategoryController::class, 'store']);
 Route::post('/category/edit', [CategoryController::class, 'edit'])->name('Category.edit');
 Route::post('/category/update',[CategoryController::class, 'update']);
 Route::post('/category/delete/{id}', [CategoryController::class, 'destroy']);
-
 //Product requests routes
 
 Route::get('/productRequestsAll', [ProductRequestController::class, 'index'])->name('ProductRequest.index');
@@ -242,6 +240,15 @@ Route::post('/CustomerDetails/edit', [Customer1Controller::class, 'edit'])->name
 Route::post('/CustomerDetails/update',[Customer1Controller::class, 'update']);
 Route::post('/CustomerDetails/delete/{id}', [Customer1Controller::class, 'destroy']);
 
+
+//Storefront routes
+Route::get('/store', function () {
+   return Inertia::render('Client/Store/Store',[]);
+});
+
+Route::get('/store/cart', function () {
+    //return Inertia::render('Client/Store/Store',[]);
+});
 
 //Storefront routes
 
